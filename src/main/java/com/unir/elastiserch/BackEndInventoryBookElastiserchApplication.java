4f2cffe8c471a882ef9c96a822ab19c687c6783d
Adriana Buenaño <adriana.buenanio@educacion.gob.ec>
2025-03-01 12:42:44 -0500

    Railway

--- src/main/java/com/unir/elastiserch/BackEndInventoryBookElastiserchApplication.java
+++ src/main/java/com/unir/elastiserch/BackEndInventoryBookElastiserchApplication.java
@@ -7,6 +7,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BackEndInventoryBookElastiserchApplication {
 
 	public static void main(String[] args) {
+		String profile = System.getenv("PROFILE");
+		System.setProperty("spring.profiles.active", profile != null ? profile : "default");
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			Thread.currentThread().interrupt();
+		}
 		SpringApplication.run(BackEndInventoryBookElastiserchApplication.class, args);
 	}
 
